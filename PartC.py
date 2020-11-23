ec3d446019eecb435c6e9d725df2dd06c63b3d77
David Lim <37480082+drlim2u@users.noreply.github.com>
2020-11-23 15:51:33 +0000

    Added code for solution to problem c2.

--- PartC.py
+++ PartC.py
@@ -1,4 +1,7 @@
-import sympy as sympy
+import sympy.core
+import sympy.functions
+import sympy.integrals
+import sympy.printing
 
 
 class PartC:
@@ -11,11 +14,23 @@ class PartC:
         A procedure to calculate the solution to problem c1.
         """
         s, t = sympy.symbols('s, t')
+
         f = sympy.ln(t**3)
         f_laplace = sympy.laplace_transform(f, t, s)
         sympy.pprint(f_laplace)
         print(sympy.latex(f_laplace))
 
+    @staticmethod
+    def problem_c2():
+        omega, t = sympy.symbols('omega t')
+
+        f = sympy.Abs(sympy.cos(omega))
+        f_laplace = sympy.laplace_transform(f, t, omega)
+        # f_laplace.pprint(f_laplace)  # This will not work as omega is not printable.
+        # # Change the sympy symbol in line 27 for pretty print.
+        print(sympy.latex(f_laplace))
+
 
 if __name__ == '__main__':
     PartC.problem_c1()
+    PartC.problem_c2()
