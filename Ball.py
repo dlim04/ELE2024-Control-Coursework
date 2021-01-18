8f3ee17d411cdbd5a44af74db1462ff81c8c39cb
David Lim <37480082+drlim2u@users.noreply.github.com>
2021-01-18 23:05:08 +0000

    Updated to use SI units

--- Ball.py
+++ Ball.py
@@ -2,11 +2,11 @@ class Ball:
     """
     Class to define the ball object which stores the instance variables for the ball in the model.
     """
-    def __init__(self, mass=425, radius=12.5):
+    def __init__(self, mass=0.425, radius=0.125):
         """
         Constructor for the ball object.
-        :param mass: (m) The mass of the ball in grams as an integer
-        :param radius: (r) The radius of the ball in centimeters as a float
+        :param mass: (m) The mass of the ball in kilograms as a float
+        :param radius: (r) The radius of the ball in meters as a float
         """
         self.mass = mass
         self.radius = radius
