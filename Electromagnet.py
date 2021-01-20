90a83507ec463a5e6406f7a04950589cfc89a0db
David Lim <37480082+drlim2u@users.noreply.github.com>
2021-01-20 06:07:36 +0000

    Corrected SI unit error

--- Electromagnet.py
+++ Electromagnet.py
@@ -4,14 +4,14 @@ class Electromagnet:
     electromagnet.
     """
     def __init__(self, nominal_inductance=120, inductance1=25, inductance_constant=1.2, resistance=53,
-                 magnetic_constant=6815):
+                 magnetic_constant=6.815):
         """
         Constructor for the Electromagnet object.
         :param nominal_inductance: (L_0) The nominal inductance of the inductor in millihenries as an integer
         :param inductance1: (L_1) The second component of the inductance of the inductor in millihenries as an integer
         :param inductance_constant: (alpha) The inductance constant as a float
         :param resistance: (R) The resistance of the electromagnet in ohms as an integer
-        :param magnetic_constant: (c) The magnetic constant in ((g m^3)/(A^2 s^2)) as an integer
+        :param magnetic_constant: (c) The magnetic constant in ((kg m^3)/(A^2 s^2)) as an integer
         """
         self.nominal_inductance = nominal_inductance
         self.inductance1 = inductance1
