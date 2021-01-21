7815a1ababc5781e788177b52ac2208ee57512ab
David Lim <37480082+drlim2u@users.noreply.github.com>
2021-01-21 11:52:02 +0000

    Corrected error where program used d value instead of delta value

--- NonLinearSystem.py
+++ NonLinearSystem.py
@@ -12,10 +12,7 @@ class NonLinearSystem(System):
         x2 = z[1]
         I = z[2]
         return [x2,
-                (5 * (((self.electromagnet.magnetic_constant * I ** 2) / (
-                            (self.d - self.x) ** 2)) + self.ball.mass * self.gravitational_acceleration * sin(
-                    self.plane_angle) - self.spring.stiffness * (
-                                  self.x - self.d) - self.spring.damping_coefficient * x1)) / (3 * self.ball.mass),
+                (5 * (((self.electromagnet.magnetic_constant * I ** 2) / ((self.delta - self.x) ** 2)) + self.ball.mass * self.gravitational_acceleration * sin(self.plane_angle) - self.spring.stiffness * (self.x - self.d) - self.spring.damping_coefficient * x1)) / (3 * self.ball.mass),
                 (voltage - (I * self.electromagnet.resistance)) / (
                             self.electromagnet.nominal_inductance + self.electromagnet.inductance1 ** (
-                                -self.electromagnet.inductance_constant * (self.d - self.x)))]
+                                -self.electromagnet.inductance_constant * (self.delta - self.x)))]
