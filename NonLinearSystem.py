6820a328e47ac1ca1db50573d0847890cf421821
David Lim <37480082+drlim2u@users.noreply.github.com>
2021-01-21 12:08:51 +0000

    Rewrote SSR in NonLinearSystem

--- NonLinearSystem.py
+++ NonLinearSystem.py
@@ -1,4 +1,4 @@
-from math import sin
+from math import sin, exp
 
 from System import System
 
@@ -12,7 +12,5 @@ class NonLinearSystem(System):
         x2 = z[1]
         I = z[2]
         return [x2,
-                (5 * (((self.electromagnet.magnetic_constant * I ** 2) / ((self.delta - self.x) ** 2)) + self.ball.mass * self.gravitational_acceleration * sin(self.plane_angle) - self.spring.stiffness * (self.x - self.d) - self.spring.damping_coefficient * x1)) / (3 * self.ball.mass),
-                (voltage - (I * self.electromagnet.resistance)) / (
-                            self.electromagnet.nominal_inductance + self.electromagnet.inductance1 ** (
-                                -self.electromagnet.inductance_constant * (self.delta - self.x)))]
+                (((5 * self.electromagnet.magnetic_constant * I)**2) / (3 * self.ball.mass * (self.delta - x1) ** 2)) + ((5 * self.gravitational_acceleration * sin(self.plane_angle)) / 3) - (((5 * self.spring.stiffness) / (3 * self.ball.mass)) * (x1 - self.d)) - ((5 * self.spring.damping_coefficient * x2) / (3 * self.ball.mass)),
+                (voltage - (I * self.electromagnet.resistance)) / (self.electromagnet.nominal_inductance + self.electromagnet.inductance1 * exp(-self.electromagnet.inductance_constant * (self.delta - self.x)))]
